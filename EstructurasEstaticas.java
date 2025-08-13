a5109abb20bb65b3a248d3b129417614a6fc7bc4
Juherles <juhe2512@gmail.com>
2025-08-13 15:30:39 -0500

    Primeros ejemplos de estructuras estáticas

---
+++ EstructurasEstaticas.java
@@ -0,0 +1,11 @@
+public class EstructurasEstaticas {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 11;
+
+        int c = 10, d = 11;
+        /*Las estructuras estáticas no se pueden modificar
+
+        */
+    }
+}
